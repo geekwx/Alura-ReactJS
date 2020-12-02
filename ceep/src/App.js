18aeb20aa5ab7cc7f352192ce98f30ac4cddd88d
Suporte UBB <suporte@ubbonline.org.br>
2020-12-02 17:30:39 -0300

    Aula 4

--- ceep/src/App.js
+++ ceep/src/App.js
@@ -1,19 +1,34 @@
-import React, { Component } from 'react';
-import  FormularioCadastro  from './components/FormularioCadastro';
-import  ListaDeNotas   from './components/ListaDeNotas';
+import React, { Component } from "react";
+import ListaDeNotas from "./components/ListaDeNotas";
+import FormularioCadastro from "./components/FormularioCadastro";
+import "./assets/App.css";
+import './assets/index.css';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+       notas:[]
+    };
 
-
- class App extends Component {
-  
-  render(){
-  return (
-    <section>
-    <FormularioCadastro />
-    <ListaDeNotas />
-
-    </section>
-  );
-}
+  }
+  criarNota(titulo, texto){
+    const novaNota = {titulo, texto};
+    const novoEstado = {
+      notas:novoArrayNotas
+    }
+    this.notas.push(novaNota);
+    this.setState({
+      notas:this.notas
+    })
+  }
+  render() {
+    return (
+      <section className="conteudo">
+        <FormularioCadastro  criarNota={this.criarNota.bind(this)}/>
+        <ListaDeNotas notas={this.notas} />
+      </section>
+    );
+  }
 }
 
 export default App;
--- ceep/src/components/CardNota/CardNota.jsx
+++ ceep/src/components/CardNota/CardNota.jsx
@@ -1,15 +1,16 @@
-import React, { Component } from 'react'; 
-import "./estilo.css"
-
+import React, { Component } from "react";
+import "./estilo.css";
 class CardNota extends Component {
-    render() { 
-        return (  <section className="card-nota">
-            <header>
-              <h3>Titulo</h3>
-              <p>Escreva sua nota</p>
-            </header>
-          </section> );
-    }
+  render() {
+    return (
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
+        </header>
+        <p className="card-nota_texto">Escreva sua nota</p>
+      </section>
+    );
+  }
 }
- 
+
 export default CardNota;
